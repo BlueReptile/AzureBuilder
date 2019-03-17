296fe817806d4824624f9e9bfb290a8e22336f98
BlueReptile <solowingphanzer@gmail.com>
2019-03-17 01:48:40 -0300

    update status card and a few more things

--- js.js
+++ js.js
@@ -4,13 +4,57 @@
     var AzureBuilder = angular.module('AzureBuilder', ['ngMaterial', 'ngMessages','chart.js']);
     AzureBuilder.controller('AzureCtrl',function($scope, $http){
 
-      $scope.labels =["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
+      $scope.labels =["Health", "Reload", "Firepower", "Torpedo", "Speed", "Anti Air", "Air Power","Oil Usage","Anti Sub"];
 
-      $scope.data = [
-      [65, 59, 90, 81, 56, 55, 40],
-      [28, 48, 40, 19, 96, 27, 100]
-      ];
+      $scope.stats = {};
 
+      $scope.$watch('selectedItem', function() {
+        if($scope.selectedItem){
+          $scope.stats = {
+            health: {
+              base: $scope.selectedItem.max.health,
+              equip: 1000,
+              total: ($scope.selectedItem.max.health + 1000)
+            },
+            reload: {
+              base: $scope.selectedItem.max.reload,
+              equip: 0,
+              total: $scope.selectedItem.max.reload + 0
+            },
+            firepower: {
+              base: $scope.selectedItem.max.firepower,
+              equip: 0,
+              total: $scope.selectedItem.max.firepower + 0
+            },
+            torpedo: {
+              base: $scope.selectedItem.max.torpedo,
+              equip: 0,
+              total: $scope.selectedItem.max.torpedo + 0
+            },
+            speed: {
+              base: $scope.selectedItem.max.speed,
+              equip: 0,
+              total: $scope.selectedItem.max.speed + 0
+            },
+            anti_air: {
+              base: $scope.selectedItem.max.anti_air,
+              equip: 0,
+              total: $scope.selectedItem.max.anti_air + 0
+            },
+            oil_usage: {
+              base: $scope.selectedItem.max.oil_usage,
+              equip: 0,
+              total: $scope.selectedItem.max.oil_usage + 0
+            },
+            anti_sub: {
+              base: $scope.selectedItem.max.anti_sub,
+              equip: 0,
+              total: $scope.selectedItem.max.anti_sub + 0
+            }
+          }
+        }
+
+      });
 
   $scope.query = function(searchText) {
     return $http
