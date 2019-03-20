cb236c0d4dbaa77a8febf78f5802252a1030544e
BlueReptile <solowingphanzer@gmail.com>
2019-03-20 01:53:59 -0300

    implement the rest of the main guns and torpeoes equipament selector

--- js.js
+++ js.js
@@ -277,7 +277,7 @@
         });
       }
 
-      $scope.GetAllEquipament = function () {
+      $scope.GetAllDestroyer = function () {
         return $http
           .get('https://cors-anywhere.herokuapp.com/https://azurlane.koumakan.jp/List_of_Destroyer_Guns')
           .then(function (data) {
@@ -287,56 +287,12 @@
             let MaxTable = tables[1];
             let rows = MaxTable.getElementsByTagName('tr');
             for (let i = 0; i < rows.length; i++) {
-              var col = rows[i];
+              let col = rows[i];
               let item = col.getElementsByTagName('td');
               let equipament = {};
               for (let index = 0; index < item.length; index++) {
                 let element = item[index];
                 switch (index) {
-                  /*
-                0 - nome
-                1 - tipo
-                2 imagem
-                3 estrela
-                4 nacionalidade
-                5 firepower
-                6 anti air
-                7 damage
-                8 coef
-                9 R
-                10 vt
-                11 reload por segundo
-                12 DPS light armor
-                13 DPS medium armor
-                14 DPS heavy armor
-                15 range
-                16 angle
-                17 atributte
-                18 ammo type
-                19 drop
-                20 notes
-
-
-                
-              {
-                name: 'teste1',
-                type: 'Auxiliary Equipment',
-                image: 'https://azurlane.koumakan.jp/w/images/8/82/7320.png',
-                rarity: '',
-                nationality:'',
-                damage : 0,
-                coef:'',
-                health: 10,
-                reload: 10,
-                firepower: 10,
-                torpedo: 10,
-                speed: 10,
-                anti_air: 10,
-                air_power: 10,
-                oil_usage: 10,
-                anti_sub: 10
-              }
-                */
                   case 0:
                     equipament.name = element.innerText;
                     break;
@@ -389,7 +345,7 @@
                     equipament.spread = element.innerText;
                     break;
                   case 17:
-                    equipament.spread = element.innerText;
+                    equipament.angle = element.innerText;
                     break;
                   case 18:
                     equipament.attribute = element.innerText;
@@ -417,5 +373,393 @@
           });
       };
 
-      $scope.GetAllEquipament();
+      $scope.GetAllLightCruiser = function () {
+        return $http
+          .get('https://cors-anywhere.herokuapp.com/https://azurlane.koumakan.jp/List_of_Light_Cruiser_Guns')
+          .then(function (data) {
+            let parser = new DOMParser();
+            let htmlDoc = parser.parseFromString(data.data, 'text/html');
+            let tables = htmlDoc.getElementsByClassName("tabbertab");
+            let MaxTable = tables[1];
+            let rows = MaxTable.getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+              let col = rows[i];
+              let item = col.getElementsByTagName('td');
+              let equipament = {};
+              for (let index = 0; index < item.length; index++) {
+                let element = item[index];
+                switch (index) {
+                  case 0:
+                    equipament.name = element.innerText;
+                    break;
+                  case 1:
+                    equipament.type = 'CL Main Guns';
+                    break;
+                  case 2:
+                    equipament.image = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 3:
+                    equipament.rarity = element.innerText;
+                    break;
+                  case 4:
+                    equipament.nationality = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 5:
+                    equipament.firepower = parseInt(element.innerText);
+                    break;
+                  case 6:
+                    equipament.anti_air = parseInt(element.innerText);
+                    break;
+                  case 7:
+                    equipament.damage = parseInt(element.innerText);
+                    break;
+                  case 8:
+                    equipament.coef = element.innerText;
+                    break;
+                  case 9:
+                    equipament.rate = element.innerText;
+                    break;
+                  case 10:
+                    equipament.VTS = element.innerText;
+                    break;
+                  case 11:
+                    equipament.reloadS = element.innerText;
+                    break;
+                  case 12:
+                    equipament.DPSL = element.innerText;
+                    break;
+                  case 13:
+                    equipament.DPSM = element.innerText;
+                    break;
+                  case 14:
+                    equipament.DPSH = element.innerText;
+                    break;
+                  case 15:
+                    equipament.range = element.innerText;
+                    break;
+                  case 16:
+                    equipament.spread = element.innerText;
+                    break;
+                  case 17:
+                    equipament.angle = element.innerText;
+                    break;
+                  case 18:
+                    equipament.attribute = element.innerText;
+                    break;
+                  case 19:
+                    equipament.ammo = element.innerText;
+                    break;
+                  case 20:
+                    equipament.dropLoc = element.innerText;
+                    break;
+                  case 21:
+                    equipament.notes = element.innerText;
+                    equipament.air_power = 0;
+                    equipament.anti_sub = 0;
+                    equipament.health = 0;
+                    equipament.oil_usage = 0;
+                    equipament.reload = 0;
+                    equipament.speed = 0;
+                    equipament.torpedo = 0;
+                    break;
+                }
+              }
+              $scope.equipaments.push(equipament);
+            }
+          });
+      };
+
+      $scope.GetAllHeavyCruiser = function () {
+        return $http
+          .get('https://cors-anywhere.herokuapp.com/https://azurlane.koumakan.jp/List_of_Heavy_Cruiser_Guns')
+          .then(function (data) {
+            let parser = new DOMParser();
+            let htmlDoc = parser.parseFromString(data.data, 'text/html');
+            let tables = htmlDoc.getElementsByClassName("tabbertab");
+            let MaxTable = tables[1];
+            let rows = MaxTable.getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+              let col = rows[i];
+              let item = col.getElementsByTagName('td');
+              let equipament = {};
+              for (let index = 0; index < item.length; index++) {
+                let element = item[index];
+                switch (index) {
+                  case 0:
+                    equipament.name = element.innerText;
+                    break;
+                  case 1:
+                    equipament.type = 'CA Main Guns';
+                    break;
+                  case 2:
+                    equipament.image = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 3:
+                    equipament.rarity = element.innerText;
+                    break;
+                  case 4:
+                    equipament.nationality = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 5:
+                    equipament.firepower = parseInt(element.innerText);
+                    break;
+                  case 6:
+                    equipament.anti_air = parseInt(element.innerText);
+                    break;
+                  case 7:
+                    equipament.damage = parseInt(element.innerText);
+                    break;
+                  case 8:
+                    equipament.coef = element.innerText;
+                    break;
+                  case 9:
+                    equipament.rate = element.innerText;
+                    break;
+                  case 10:
+                    equipament.VTS = element.innerText;
+                    break;
+                  case 11:
+                    equipament.reloadS = element.innerText;
+                    break;
+                  case 12:
+                    equipament.DPSL = element.innerText;
+                    break;
+                  case 13:
+                    equipament.DPSM = element.innerText;
+                    break;
+                  case 14:
+                    equipament.DPSH = element.innerText;
+                    break;
+                  case 15:
+                    equipament.range = element.innerText;
+                    break;
+                  case 16:
+                    equipament.spread = element.innerText;
+                    break;
+                  case 17:
+                    equipament.angle = element.innerText;
+                    break;
+                  case 18:
+                    equipament.attribute = element.innerText;
+                    break;
+                  case 19:
+                    equipament.ammo = element.innerText;
+                    break;
+                  case 20:
+                    equipament.dropLoc = element.innerText;
+                    break;
+                  case 21:
+                    equipament.notes = element.innerText;
+                    equipament.air_power = 0;
+                    equipament.anti_sub = 0;
+                    equipament.health = 0;
+                    equipament.oil_usage = 0;
+                    equipament.reload = 0;
+                    equipament.speed = 0;
+                    equipament.torpedo = 0;
+                    break;
+                }
+              }
+              $scope.equipaments.push(equipament);
+            }
+          });
+      };
+
+      $scope.GetAllBattleship = function () {
+        return $http
+          .get('https://cors-anywhere.herokuapp.com/https://azurlane.koumakan.jp/List_of_Battleship_Guns')
+          .then(function (data) {
+            let parser = new DOMParser();
+            let htmlDoc = parser.parseFromString(data.data, 'text/html');
+            let tables = htmlDoc.getElementsByClassName("tabbertab");
+            let MaxTable = tables[1];
+            let rows = MaxTable.getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+              let col = rows[i];
+              let item = col.getElementsByTagName('td');
+              let equipament = {};
+              for (let index = 0; index < item.length; index++) {
+                let element = item[index];
+                switch (index) {
+                  case 0:
+                    equipament.name = element.innerText;
+                    break;
+                  case 1:
+                    equipament.type = 'BB Main Guns';
+                    break;
+                  case 2:
+                    equipament.image = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 3:
+                    equipament.rarity = element.innerText;
+                    break;
+                  case 4:
+                    equipament.nationality = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 5:
+                    equipament.firepower = parseInt(element.innerText);
+                    break;
+                  case 6:
+                    equipament.anti_air = parseInt(element.innerText);
+                    break;
+                  case 7:
+                    equipament.damage = parseInt(element.innerText);
+                    break;
+                  case 8:
+                    equipament.coef = element.innerText;
+                    break;
+                  case 9:
+                    equipament.rate = element.innerText;
+                    break;
+                  case 10:
+                    equipament.VTS = element.innerText;
+                    break;
+                  case 11:
+                    equipament.reloadS = element.innerText;
+                    break;
+                  case 12:
+                    equipament.DPSL = element.innerText;
+                    break;
+                  case 13:
+                    equipament.DPSM = element.innerText;
+                    break;
+                  case 14:
+                    equipament.DPSH = element.innerText;
+                    break;
+                  case 15:
+                    equipament.range = element.innerText;
+                    break;
+                  case 16:
+                    equipament.spread = element.innerText;
+                    break;
+                  case 17:
+                    equipament.angle = element.innerText;
+                    break;
+                  case 18:
+                    equipament.attribute = element.innerText;
+                    break;
+                  case 19:
+                    equipament.ammo = element.innerText;
+                    break;
+                  case 20:
+                    equipament.dropLoc = element.innerText;
+                    break;
+                  case 21:
+                    equipament.notes = element.innerText;
+                    equipament.air_power = 0;
+                    equipament.anti_sub = 0;
+                    equipament.health = 0;
+                    equipament.oil_usage = 0;
+                    equipament.reload = 0;
+                    equipament.speed = 0;
+                    equipament.torpedo = 0;
+                    break;
+                }
+              }
+              $scope.equipaments.push(equipament);
+            }
+          });
+      };
+
+      $scope.GetAllTorpedoes = function () {
+        return $http
+          .get('https://cors-anywhere.herokuapp.com/https://azurlane.koumakan.jp/List_of_Torpedoes')
+          .then(function (data) {
+            let parser = new DOMParser();
+            let htmlDoc = parser.parseFromString(data.data, 'text/html');
+            let tables = htmlDoc.getElementsByClassName("tabbertab");
+            let MaxTable = tables[1];
+            let rows = MaxTable.getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+              let col = rows[i];
+              let item = col.getElementsByTagName('td');
+              let equipament = {};
+              for (let index = 0; index < item.length; index++) {
+                let element = item[index];
+                switch (index) {
+                  case 0:
+                    equipament.name = element.innerText;
+                    break;
+                  case 1:
+                    equipament.image = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 2:
+                    equipament.rarity = element.innerText;
+                    break;
+                  case 3:
+                    equipament.nationality = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 4:
+                    equipament.torpedo = parseInt(element.innerText);
+                    break;
+                  case 5:
+                    equipament.damage = parseInt(element.innerText);
+                    break;
+                  case 6:
+                    equipament.coef = element.innerText;
+                    break;
+                  case 7:
+                    equipament.rate = element.innerText;
+                    break;
+                  case 8:
+                    equipament.reloadS = element.innerText;
+                    break;
+                  case 9:
+                    equipament.DPSL = element.innerText;
+                    break;
+                  case 10:
+                    equipament.DPSM = element.innerText;
+                    break;
+                  case 11:
+                    equipament.DPSH = element.innerText;
+                    break;
+                    case 12:
+                    console.log(element.innerText);
+                    equipament.DPSLIT = element.innerText;
+                    break;
+                    case 13:
+                    equipament.DPSMIT = element.innerText;
+                    break;
+                    case 14:
+                    equipament.DPSHIT = element.innerText;
+                    break;
+                  case 15:
+                    equipament.range = element.innerText;
+                    break;
+                  case 16:
+                    equipament.spread = element.innerText;
+                    break;
+                  case 17:
+                    equipament.angle = element.innerText;
+                    break;
+                  case 18:
+                    equipament.attribute = element.innerText;
+                    break;
+                  case 19:
+                    equipament.dropLoc = element.innerText;
+                    break;
+                  case 20:
+                    equipament.notes = element.innerText;
+                    equipament.air_power = 0;
+                    equipament.anti_sub = 0;
+                    equipament.health = 0;
+                    equipament.oil_usage = 0;
+                    equipament.reload = 0;
+                    equipament.speed = 0;
+                    equipament.firepower = 0;
+                    equipament.type = 'Torpedoes';
+                    equipament.anti_air = 0;
+                    break;
+                }
+              }
+              $scope.equipaments.push(equipament);
+            }
+          });
+      };
+
+      $scope.GetAllDestroyer();
+      $scope.GetAllLightCruiser();
+      $scope.GetAllHeavyCruiser();
+      $scope.GetAllBattleship();
+      $scope.GetAllTorpedoes();
     });
