d02c040c40d2b0931b8d1d4dbc2c56c78d71ec3e
BlueReptile <solowingphanzer@gmail.com>
2019-03-20 19:31:31 -0300

    add the rest of the equips

--- js.js
+++ js.js
@@ -945,7 +945,6 @@
                     break;
                 }
               }
-              console.log(equipament);
               $scope.equipaments.push(equipament);
             }
           });
@@ -1042,8 +1041,151 @@
                     break;
                 }
               }
-              console.log(equipament);
               $scope.equipaments.push(equipament);
+            }
+          });
+      };
+
+      $scope.GetAllAAGuns = function () {
+        return $http
+          .get('https://cors-anywhere.herokuapp.com/https://azurlane.koumakan.jp/List_of_AA_Guns')
+          .then(function (data) {
+            let parser = new DOMParser();
+            let htmlDoc = parser.parseFromString(data.data, 'text/html');
+            let tables = htmlDoc.getElementsByClassName("tabbertab");
+            let MaxTable = tables[1];
+            let rows = MaxTable.getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+              let col = rows[i];
+              let item = col.getElementsByTagName('td');
+              let equipament = {};
+              for (let index = 0; index < item.length; index++) {
+                let element = item[index];
+                switch (index) {
+                  case 0:
+                    equipament.name = element.innerText;
+                    break;
+                  case 1:
+                    equipament.image = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 2:
+                    equipament.rarity = element.innerText;
+                    break;
+                  case 3:
+                    equipament.nationality = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 4:
+                    equipament.anti_air = parseInt(element.innerText);
+                    break;
+                  case 5:
+                    equipament.damage = parseInt(element.innerText);
+                    break;
+                  case 6:
+                    equipament.reloadS = parseInt(element.innerText);
+                    break;
+                  case 7:
+                    equipament.dps = parseInt(element.innerText);
+                    break;
+                  case 8:
+                    equipament.rng = parseInt(element.innerText);
+                    break;
+                  case 9:
+                    equipament.angle = parseInt(element.innerText);
+                    break;
+                  case 10:
+                    equipament.ammo = element.innerText;
+                    break;
+                  case 11:
+                    equipament.drop = element.innerText;
+                    break;
+                  case 12:
+                    equipament.notes = element.innerText;
+                    equipament.torpedo = 0;
+                    equipament.anti_sub = 0;
+                    equipament.health = 0;
+                    equipament.oil_usage = 0;
+                    equipament.reload = 0;
+                    equipament.speed = 0;
+                    equipament.firepower = 0;
+                    equipament.type = 'Anti-Air Guns';
+                    equipament.air_power = 0;
+                    break;
+                }
+              }
+              $scope.equipaments.push(equipament);
+            }
+          });
+      };
+
+
+      $scope.GetAllAuxiliary = function () {
+        return $http
+          .get('https://cors-anywhere.herokuapp.com/https://azurlane.koumakan.jp/List_of_Auxiliary_Equipment')
+          .then(function (data) {
+            let parser = new DOMParser();
+            let htmlDoc = parser.parseFromString(data.data, 'text/html');
+            let tables = htmlDoc.getElementsByClassName("tabbertab");
+            let MaxTable = tables[1];
+            let rows = MaxTable.getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+              let col = rows[i];
+              let item = col.getElementsByTagName('td');
+              let equipament = {};
+              for (let index = 0; index < item.length; index++) {
+                let element = item[index];
+                switch (index) {
+                  case 0:
+                    equipament.name = element.innerText;
+                    break;
+                  case 1:
+                    equipament.image = (element.getElementsByTagName('img')[0].src + '').replace('https://bluereptile.github.io/', 'https://azurlane.koumakan.jp/');
+                    break;
+                  case 2:
+                    equipament.rarity = element.innerText;
+                    break;
+                  case 3:
+                    equipament.health = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 4:
+                    equipament.firepower = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 5:
+                    equipament.anti_air = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 6:
+                    equipament.torpedo = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 7:
+                    equipament.air_power = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 8:
+                    equipament.reload = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 9:
+                    equipament.evasion = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 10:
+                    equipament.oxygen = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 11:
+                    equipament.accuracy = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 12:
+                    equipament.speed = isNaN(parseInt(element.innerText))? 0 : parseInt(element.innerText);
+                    break;
+                  case 13:
+                    equipament.notes = element.innerText;
+                    equipament.anti_sub = 0;
+                    equipament.oil_usage = 0;
+                    equipament.type = 'Auxiliary Equipment';
+                    break;
+                  case 14:
+                    equipament.drop = element.innerText;
+                    break;
+                }
+              }
+              $scope.equipaments.push(equipament);
+              console.log( $scope.equipaments);
             }
           });
       };
@@ -1056,4 +1198,6 @@
       $scope.GetAllFighters();
       $scope.GetAllBombers();
       $scope.GetAllTBombers();
+      $scope.GetAllAAGuns();
+      $scope.GetAllAuxiliary();
     });
